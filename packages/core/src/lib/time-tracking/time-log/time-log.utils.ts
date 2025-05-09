b0a6fc8af9d5bfeda2bf505ee0d0dff64c77d436
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2025-05-09 11:08:34 -0500

    fix: Fix moment import

--- packages/core/src/lib/time-tracking/time-log/time-log.utils.ts
+++ packages/core/src/lib/time-tracking/time-log/time-log.utils.ts
@@ -1,4 +1,4 @@
-import moment from 'moment';
+import * as moment from 'moment';
 import { reduce } from 'underscore';
 import { ArraySum } from '@gauzy/utils';
 import { ITimeLog, TimeLogPartialStatus, TimeLogType } from '@gauzy/contracts';
